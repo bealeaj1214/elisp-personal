8ece05a519ba06cbc8a20ae5d8ff753afd53faf8
Alan Beale <alan.beale@chicagotrading.com>
2010-10-29 09:01:04 -0500

    refactor to common function

--- merge-helper.el
+++ merge-helper.el
@@ -4,13 +4,14 @@
 (defun nuke-region-b()
   "Deletes region-b from merge result - git helper function."
   (interactive)
-  (search-forward "====")
-  (move-beginning-of-line nil)
-  (let ((start   (point)))
-    (search-forward ">>>>")
-    (move-end-of-line nil)
-    (delete-region start (point)))
-  nil)
+  ;; (search-forward "====")
+  ;; (move-beginning-of-line nil)
+  ;; (let ((start   (point)))
+  ;;   (search-forward ">>>>")
+  ;;   (move-end-of-line nil)
+  ;;   (delete-region start (point)))
+  ;; nil)
+  (merge-helper-nuke-region "====" ">>>>"))
 
 
 ;
@@ -19,10 +20,24 @@
   (defun nuke-region-a()
   "Deletes region-a from merge result - git helper function."
   (interactive)
-  (search-forward "<<<<")
-  (move-beginning-of-line nil)
-  (let ((start   (point)))
-    (search-forward "====")
-    (move-end-of-line nil)
-    (delete-region start (point)))
-  nil)
+  ;; (search-forward "<<<<")
+  ;; (move-beginning-of-line nil)
+  ;; (let ((start   (point)))
+  ;;   (search-forward "====")
+  ;;   (move-end-of-line nil)
+  ;;   (delete-region start (point)))
+  ;; nil)
+  (merge-helper-nuke-region "<<<<" "===="))
+
+ (defun merge-helper-nuke-region (nuke-region-start nuke-region-end)
+   "Deletes region from megre result need two parameters."
+   
+   (save-match-data
+     (search-forward nuke-region-start)
+     (move-beginning-of-line nil)
+     (let ((start   (point)))
+       (search-forward nuke-region-end)
+       (move-end-of-line nil)
+       (delete-region start (point))))
+   
+   nil)
