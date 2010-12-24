e82f3aa674349f921505fb7f6b5a5c48aeafe8e4
Alan Beale <alan.beale@chicagotrading.com>
2010-12-24 10:57:58 -0600

    working simple regexp

--- setupJavaClass.el
+++ setupJavaClass.el
@@ -1,6 +1,15 @@
 (defun setup-java-class()
   "creates a java class skelton based on buffer-file name"
   (interactive "*")
-  (let     ((target-file-name (buffer-file-name nil)))
-    (message "working on file %s " target-file-name))
-)
+  (let     ((target-file-name (buffer-file-name nil))
+	    (test-regexp    ".java$")
+	    )
+    ;;(message "working on file %s " target-file-name))
+
+    ;;(message (regexp-quote "([A-Za-z][a-zA-Z0-9_]*).java$"))
+    (if (string-match test-regexp target-file-name)
+	(message (format "found match in %s" target-file-name))
+      (message "not match found"))
+
+
+))
