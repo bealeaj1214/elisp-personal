7329f09f73a4df56e781a8bb24a8565f2a2e2c78
Alan Beale <alan.beale@chicagotrading.com>
2010-12-24 12:45:50 -0600

    Nearly working version   - use replace-regexp-in-string to update replace "/" with "."   - use let subexpression to create target-package-name   - use format to create class skeleton

--- setupJavaClass.el
+++ setupJavaClass.el
@@ -9,9 +9,12 @@
     (if (string-match test-regexp target-file-name)
 	(let    ((target-package-path (match-string 2 target-file-name) )
 		 (target-class-name (match-string 3 target-file-name) ) )
-	(message (format "found class %s  - package directory - %s" 
-			 target-class-name
-			 target-package-path ))
+	  (let ((target-package-name (replace-regexp-in-string "/" "." target-package-path nil nil nil)) 	
+		)
+	    (message (format "package %s;\n\n\n\npublic class %s { \n\n\n\n}" 
+			     
+			     target-package-name target-class-name))
+	    ) 
 	)
 	
       (message "not match found"))
