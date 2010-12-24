22e86f0cae0ebcd85233eb80606d83c019019ad7
Alan Beale <alan.beale@chicagotrading.com>
2010-12-24 11:28:53 -0600

    working version

--- setupJavaClass.el
+++ setupJavaClass.el
@@ -2,14 +2,15 @@
   "creates a java class skelton based on buffer-file name"
   (interactive "*")
   (let     ((target-file-name (buffer-file-name nil))
-	    (test-regexp    "\\([a-zA-Z][a-zA-Z0-9_]*\\).java$")
+	    (test-regexp    "\\(src/.*/java/\\|src/\\)\\(.*\\)/\\([a-zA-Z][a-zA-Z0-9_]*\\).java$")
 	    )
     ;;(message "working on file %s " target-file-name))
 
     ;;(message (regexp-quote "([A-Za-z][a-zA-Z0-9_]*).java$"))
     (if (string-match test-regexp target-file-name)
-	(message (format "found class in %s" 
-			 (match-string 1 target-file-name)))
+	(message (format "found class %s  - package directory - %s" 
+			 (match-string 3 target-file-name)
+			 (match-string 2 target-file-name) ))
       (message "not match found"))
 
 
