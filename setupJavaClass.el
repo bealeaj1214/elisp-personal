101bab56d77e2a6ddd5a9fb8e7b0a1b7b2a9d4e2
Alan Beale <alan.beale@chicagotrading.com>
2010-12-24 11:43:40 -0600

    Added load-library to sJC

--- setupJavaClass.el
+++ setupJavaClass.el
@@ -4,13 +4,15 @@
   (let     ((target-file-name (buffer-file-name nil))
 	    (test-regexp    "\\(src/.*/java/\\|src/\\)\\(.*\\)/\\([a-zA-Z][a-zA-Z0-9_]*\\).java$")
 	    )
-    ;;(message "working on file %s " target-file-name))
+    ;;(require 'replace)
+    (load-library "replace")
 
     ;;(message (regexp-quote "([A-Za-z][a-zA-Z0-9_]*).java$"))
     (if (string-match test-regexp target-file-name)
 	(message (format "found class %s  - package directory - %s" 
 			 (match-string 3 target-file-name)
 			 (match-string 2 target-file-name) ))
+	
       (message "not match found"))
 
 
