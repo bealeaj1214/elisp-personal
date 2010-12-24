45a80f812c323f1a4795d95e721569fafcb6f897
Alan Beale <alan.beale@chicagotrading.com>
2010-12-24 11:55:08 -0600

    Working version of the regexp to select both package path and class name

--- setupJavaClass.el
+++ setupJavaClass.el
@@ -2,16 +2,16 @@
   "creates a java class skelton based on buffer-file name"
   (interactive "*")
   (let     ((target-file-name (buffer-file-name nil))
-	    (test-regexp    "\\(src/.*/java/\\|src/\\)\\(.*\\)/\\([a-zA-Z][a-zA-Z0-9_]*\\).java$")
+	    (test-regexp    "\\(src/.+/java\\|/src\\)/\\(.*\\)/\\([a-zA-Z][a-zA-Z0-9_]*\\).java$")
 	    )
-    ;;(require 'replace)
-    (load-library "replace")
 
     ;;(message (regexp-quote "([A-Za-z][a-zA-Z0-9_]*).java$"))
     (if (string-match test-regexp target-file-name)
+	(let    ((target-package-name (match-string 2 target-file-name) ))
 	(message (format "found class %s  - package directory - %s" 
 			 (match-string 3 target-file-name)
-			 (match-string 2 target-file-name) ))
+			 target-package-name ))
+	)
 	
       (message "not match found"))
 
