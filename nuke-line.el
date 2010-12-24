2da96f2f5282b8cd0f5b0d1f0380f599f4b6b9e5
Alan Beale <alan.beale@chicagotrading.com>
2010-12-24 15:40:25 -0600

    Interactive function to remove a line

--- nuke-line.el
+++ nuke-line.el
@@ -1,5 +1,17 @@
 (defun nuke-line-interactive(nuke-string)
   " find and delete all lines containing nuke-string "
   (interactive "*s enter string: ")
-  (message (format "you entered %s" nuke-string))
+  ;;(message (format "you entered %s" nuke-string))
+  (save-excursion
+    (save-restriction 
+      (save-match-data
+	(widen) 
+	(goto-char (point-min))
+	(while (search-forward nuke-string nil t)
+	  (beginning-of-line)
+	  (let ((start  (point)))
+	    (end-of-line)
+	    (delete-region start (+ (point) 1))
+	    ) ) ) ) )
+
 )
